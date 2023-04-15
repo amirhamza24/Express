4b508b4619516589476e171bf84d4a3a3ff6d3c3
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-04-16 01:41:33 +0600

    Add files via upload

---
+++ Mod 58/githubAndEmailVerificationWithFirebase/src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBq_UaXSjSicP9SE520j6VxhltC7XJOxOQ",
+    authDomain: "simple-firebase-783cf.firebaseapp.com",
+    projectId: "simple-firebase-783cf",
+    storageBucket: "simple-firebase-783cf.appspot.com",
+    messagingSenderId: "1044414412993",
+    appId: "1:1044414412993:web:d92068211e9a935f2c3c40",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
